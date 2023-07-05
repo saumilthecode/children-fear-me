205f5314da92db8a940e825556349460359aac7a
potato dog man <saumil707@gmail.com>
2023-07-05 16:21:49 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,11 @@
 let noise = 0
-let light2 = 0
-led.enable(false)
+led.enable(true)
 basic.forever(function () {
-    light2 = smarthome.ReadLightIntensity(AnalogPin.P3)
-    if (light2 < 50) {
-        noise = smarthome.ReadLightIntensity(AnalogPin.P2)
-        if (noise > 70) {
-        	
-        }
+    noise = smarthome.ReadNoise(AnalogPin.P2)
+    if (noise > 20) {
+        neopixel.create(DigitalPin.P1, 1, NeoPixelMode.RGB).showColor(neopixel.colors(NeoPixelColors.Indigo))
+        basic.pause(1000)
+        neopixel.create(DigitalPin.P1, 1, NeoPixelMode.RGB).showColor(neopixel.colors(NeoPixelColors.White))
+        basic.pause(1000)
     }
 })

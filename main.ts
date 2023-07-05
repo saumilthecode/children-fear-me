c7192d3b36354d509fa9728549296fbece7653df
potato dog man <saumil707@gmail.com>
2023-07-05 16:29:03 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,10 +2,5 @@ let noise = 0
 led.enable(true)
 basic.forever(function () {
     noise = smarthome.ReadNoise(AnalogPin.P2)
-    if (noise > 20) {
-        neopixel.create(DigitalPin.P1, 1, NeoPixelMode.RGB).showColor(neopixel.colors(NeoPixelColors.Indigo))
-        basic.pause(1000)
-        neopixel.create(DigitalPin.P1, 1, NeoPixelMode.RGB).showColor(neopixel.colors(NeoPixelColors.White))
-        basic.pause(1000)
-    }
+    basic.showString("" + (noise))
 })
